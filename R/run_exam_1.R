ff5132c3167fbfaec61650e2873a986d5bfaa5aa
pbenavides <pbenavides@iteso.mx>
2024-02-27 08:14:53 -0600

    Edited example

--- R/run_exam_1.R
+++ R/run_exam_1.R
@@ -4,7 +4,7 @@
 #' @export
 #'
 #' @examples
-#' run_exam_1()
+#' # run_exam_1()
 run_exam_1 <- function() {
   quarto::quarto_serve("inst/shiny-exams/parcial_1/parcial_1.qmd",
                        browse = TRUE)
