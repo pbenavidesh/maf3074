ab8cadfe4312f32c8fd994429333c968afdc0c5e
pbenavides <pbenavides@iteso.mx>
2024-02-27 08:08:25 -0600

    Added roxygen skeleton and created documentation.

--- R/run_exam_1.R
+++ R/run_exam_1.R
@@ -1,4 +1,10 @@
+#' Run the exam
+#'
+#' @return
 #' @export
+#'
+#' @examples
+#' run_exam_1()
 run_exam_1 <- function() {
   quarto::quarto_serve("inst/shiny-exams/parcial_1/parcial_1.qmd",
                        browse = TRUE)
